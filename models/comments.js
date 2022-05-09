c21aeb6f0e758312aa18bc754a70fa805b7ab082
Shane Brooks <shanedbrooks06@gmail.com>
2022-05-09 15:38:48 -0400

    Pic&Comment Schemas fix

--- models/comments.js
+++ models/comments.js
@@ -2,8 +2,9 @@ const { Schema } = require('mongoose')
 
 const Comment = new Schema(
     {
-        name: { type: String, required: true },
-        description: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    picture: { type: Schema.Types.ObjectId, ref: 'pictures' }
 }, 
     { timestamps: true }
     )
