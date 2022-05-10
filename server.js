b155a2793a43c7f8a83ab465bdff7c10b491a0c5
Shane Brooks <shanedbrooks06@gmail.com>
2022-05-09 20:08:01 -0400

    GET COMMENT route working

--- server.js
+++ server.js
@@ -3,12 +3,19 @@ const cors = require('cors')
 const logger = require('morgan')
 const PORT = process.env.PORT || 3001
 const db = require('./db')
+const { Comment } = require('./models')
+
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
+
+app.get('/comments', async (req, res) => {
+    const comments = await Comment.find()
+    res.json(comments)
+})
 
 app.get('/', (req, res) => {
   res.send(' UNDRWRLD MAG!')
